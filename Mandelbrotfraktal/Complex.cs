184b5d8800c510acd201e7370ebba9c7317c98b1
Peter Lustig <daniel.zirke@gmail.com>
2019-09-13 10:36:22 +0200

    Projektdateien hinzufügen.

---
+++ Mandelbrotfraktal/Complex.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrotfraktal
+{
+    class Complex
+    {
+
+        private double real;
+        private double imag;
+
+
+        public double Real
+        {
+            get { return real; }
+            set { real = value; }
+        }
+
+
+        public double Imag
+        {
+            get { return imag; }
+            set { imag = value; }
+        }
+
+        public Complex()
+        {
+            real = imag = 0.0;
+        }
+
+        public Complex(double real, double imag)
+        {
+            Real = real;
+            Imag = imag;
+        }
+
+        public Complex(Complex c)
+        {
+            Real = c.Real;
+            Imag = c.Imag;
+        }
+
+        public double Argument
+        {
+            get { return Math.Sqrt(Real * Real + Imag * Imag); }
+        }
+
+        public Complex Add(Complex c)
+        {
+            this.Real += c.Real;
+            this.Imag += c.Imag;
+            return this;
+        }
+
+        public Complex Sub(Complex c)
+        {
+            this.Real -= c.Real;
+            this.Imag -= c.Imag;
+            return this;
+        }
+        public Complex Mul(Complex c)
+        {
+            double real = this.Real;
+            double imag = this.Imag;
+            this.Real = real * c.Real - imag * c.Imag;
+            this.Imag = real * c.Imag + imag * c.Real;
+            return this;
+        }
+        public static Complex operator + (Complex a, Complex b)
+        {
+            Complex c = new Complex(a);
+            c.Add(b);
+            return c;
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            Complex c = new Complex(a);
+            c.Sub(b);
+            return c;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            Complex c = new Complex(a);
+            c.Mul(b);
+            return c;
+        }
+
+
+    }
+}
---
+++ Mandelbrotfraktal/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrotfraktal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex Coord = new Complex(-0.6, 1.2);
+            Complex Temp;
+            int iterations;
+
+            while(Coord.Imag >= -1.2)
+            {
+                while(Coord.Real <= 1.77)
+                {
+                    iterations = 0;
+                    Temp = new Complex(Coord);
+
+                    while (Temp.Argument < 4 && ++iterations < 50)
+                        Temp = Temp * Temp + Coord;
+
+                    switch (iterations % 5)
+                    {
+                        case 0:
+                            Console.Write(' ');
+                            break;
+                        case 1:
+                            Console.Write('.');
+                            break;
+                        case 2:
+                            Console.Write('o');
+                            break;
+                        case 3:
+                            Console.Write('O');
+                            break;
+                        case 4:
+                            Console.Write('@');
+                            break;
+                    }
+
+                    Coord.Real += 0.03;
+                    Console.WriteLine();
+                    Coord.Imag += 0.05;
+                    Coord.Real = -0.6;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
